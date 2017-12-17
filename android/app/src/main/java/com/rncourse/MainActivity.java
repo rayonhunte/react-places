728c0747f60f0fe4105cceb3f4a76076984d4767
Rayon Hunte <rayon.hunte@gmail.com>
2017-12-17 01:55:58 -0400

    before app wipe

--- android/app/src/main/java/com/rncourse/MainActivity.java
+++ android/app/src/main/java/com/rncourse/MainActivity.java
@@ -1,15 +1,8 @@
 package com.rncourse;
 
-import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.controllers.SplashActivity;
 
-public class MainActivity extends ReactActivity {
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "rncourse";
-    }
+public class MainActivity extends  SplashActivity {
+
 }
