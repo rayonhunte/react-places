01278d3cbac7fdbef6dde69b0b893b44dc875fe6
Rayon Hunte <rayon.hunte@gmail.com>
2017-12-28 22:33:11 -0400

    before deploy

--- android/app/src/main/java/com/rncourse/MainActivity.java
+++ android/app/src/main/java/com/rncourse/MainActivity.java
@@ -1,8 +1,27 @@
 package com.rncourse;
 
 import com.reactnativenavigation.controllers.SplashActivity;
+import android.widget.LinearLayout;
+import android.graphics.Color;
+import android.widget.TextView;
+import android.view.Gravity;
+import android.util.TypedValue;
 
 
 public class MainActivity extends  SplashActivity {
+    @Override 
+    public LinearLayout createSplashLayout(){
+        LinearLayout view = new LinearLayout(this);
+        TextView textView = new TextView(this);
+        view.setBackgroundColor(Color.parseColor("#521751"));
+        view.setGravity(Gravity.CENTER);
 
+        textView.setTextColor(Color.parseColor("#fa923f"));
+        textView.setText("Awesome Places");
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 40);
+
+        view.addView(textView);
+        return view;
+    }
 }
